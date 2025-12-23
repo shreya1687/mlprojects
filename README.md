c84d8cde1ef977aac2bbba85e24dfda2f55912d1
shreya1687 <shreya1687@gmail.com>
2025-12-23 05:32:57 +0530

    setup

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 End to End Machine Learning project
+
+Agenda:
+1- Setup the git hub {Repoository}
+a) Build a new environment
+b) setup.py
+c) requirments.txt
+
+2- src folder and build the package
+ 
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages,setup
+from typing import List
+HYPHEN_E_DOT='-e .'
+
+def get_requirements(file_path:str) -> List[str]:
+    '''
+    This function will return the list of requirements
+    
+    :param file_path: requirements.txt
+    :type file_path: str
+    :return: Description
+    :rtype: List[str]
+    '''
+    
+    requirements=[]
+    with open(file_path, 'r') as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    
+    return requirements
+        
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Shreya',
+    author_email='shreya1687@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
